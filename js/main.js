85c7b1cdc88ee715cf5a69e2d6316b7f65cf313e
Borja <pintocorreas@gmail.com>
2023-09-25 16:00:00 +0200

    Ejercicio 1 JS2

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// KATA DNI
+
+// function letraDNI(numeroDni){
+//     var numeroDni = parseInt(prompt("Dime los numeros de tu DNI"))
+//     var letra = numeroDni % 23
+//     const letras = "TRWAGMYFPDXBNJZSQVHLCKET".split("");
+
+//     return letras[letra]
+// }
+
+// console.log(letraDNI());
+
+//Ejercicio 1
+
+var figura = prompt("Dime si es un cuadrado, circulo o triangulo")
+
+if(figura === "cuadrado"){
+    var lado = parseInt(prompt("Dime el lado del cuadrado"))
+            console.log(lado * lado);
+}else if(figura === "circulo"){
+    var radio = parseInt(prompt("Dime el radio del circulo"))
+            console.log((radio ** 2) * Math.PI)
+}else if(figura === "triangulo"){
+    var base = parseInt(prompt("Dime la base del triangulo"))
+            var altura = parseInt(prompt("Dime la altura del triangulo"))
+            console.log((base * altura) / 2)
+}
+
