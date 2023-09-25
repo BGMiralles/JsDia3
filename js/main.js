042afc05fe3e9cf60797cb313cff4acd7511e5ff
Borja <pintocorreas@gmail.com>
2023-09-25 16:22:17 +0200

    Ejercicio 3

--- js/main.js
+++ js/main.js
@@ -12,17 +12,31 @@
 
 //Ejercicio 1
 
-var figura = prompt("Dime si es un cuadrado, circulo o triangulo")
+// var figura = prompt("Dime si es un cuadrado, circulo o triangulo")
 
-if(figura === "cuadrado"){
-    var lado = parseInt(prompt("Dime el lado del cuadrado"))
-            console.log(lado * lado);
-}else if(figura === "circulo"){
-    var radio = parseInt(prompt("Dime el radio del circulo"))
-            console.log((radio ** 2) * Math.PI)
-}else if(figura === "triangulo"){
-    var base = parseInt(prompt("Dime la base del triangulo"))
-            var altura = parseInt(prompt("Dime la altura del triangulo"))
-            console.log((base * altura) / 2)
+// if(figura === "cuadrado"){
+//     var lado = parseInt(prompt("Dime el lado del cuadrado"))
+//             console.log(lado * lado);
+// }else if(figura === "circulo"){
+//     var radio = parseInt(prompt("Dime el radio del circulo"))
+//             console.log((radio ** 2) * Math.PI)
+// }else if(figura === "triangulo"){
+//     var base = parseInt(prompt("Dime la base del triangulo"))
+//             var altura = parseInt(prompt("Dime la altura del triangulo"))
+//             console.log((base * altura) / 2)
+// }
+
+//Ejercicio 3
+
+function numeroPrimo(num1){
+    var num1 = parseInt(prompt("Dame un numero"))
+    if(num1 / num1 === 1 & num1 / 1 === num1){
+        console.log("true");
+    }else{
+        console.log("false");
+    }
+    
+
 }
 
+numeroPrimo()
